25d4de091bcfaa59a5c7f9188408d09dac53e75e
Burak Ugur <42806395+burakugurr@users.noreply.github.com>
2021-02-02 04:27:21 +0300

    add files

---
+++ KDV2.c
@@ -0,0 +1,34 @@
+	/*
+	Name: CALCULATION of KDV with Dollar Exchange Rate
+	Copyright: 
+	Author: BURAK UÐUR
+	Date: 23.10.18 19:46
+	Description: Hacetepe Universty Departman of Statistics C Projeckt-2
+*/
+
+#include<stdio.h>
+#include<locale.h>
+#include <time.h>
+
+
+int main(){
+	float dolarf,dolar,tlf,fiyat;
+	setlocale(LC_ALL, "Turkish"); 
+	printf("Ürün fiyatý girin: $");
+	scanf("%f",&dolarf);
+	printf("\n");
+	
+time_t simdiki_zaman ;
+simdiki_zaman = time(NULL) ;
+printf(ctime(&simdiki_zaman ) ) ;
+
+	printf("Tarihindeki Dolar Kurunu giriniz:");
+	scanf("%f",&dolar);
+	
+	tlf=dolar*dolarf;
+	fiyat=tlf+tlf*0.18;
+
+	printf("KDVli SATIÞ FÝYATI : %.2f",fiyat);
+	
+	return 0 ;
+}
